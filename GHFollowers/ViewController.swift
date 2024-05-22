b90cccb61f4b32064ff1e4ab3886a0749d6e20a3
ashon96 <andrew.shon@willowtreeapps.com>
2024-05-22 12:15:46 -0400

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Andrew Shon on 5/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
